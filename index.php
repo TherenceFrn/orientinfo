f61700bdb79311f2a6c8e6183a088b8d22e34fbc
FERRON Therence <therence.ferron@orange.fr>
2019-01-09 12:21:56 +0100

    Index - Nav + Header ( html )

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title>Orient'Info</title>
+
+  </head>
+
+  <body>
+
+      <header class="header">
+
+      <nav>
+
+        <ul class="navigation">
+
+          <li class="navigation-bouton">Acceuil</li>
+
+          <li class="navigation-bouton">Magazine</li>
+
+          <li class="navigation-bouton">Les Filières</li>
+
+          <li class="navigation-bouton">Les Ecoles</li>
+
+          <li class="navigation-bouton">Temoignages</li>
+
+          <li class="navigation-bouton">Contact</li>
+
+          <li class="navigation-bouton">Etudes</li>
+
+          <li class="navigation-bouton">Profil</li>
+
+        </ul>
+
+      </nav>
+
+      <section class="section section-1">
+
+      <a href="index.php"><h1>Orient'Info</h1></a>
+
+      <h2>Le site fait pour votre orientation</h2>
+
+      </section>
+
+      </header>
+
+      <section class="section section-2">
+
+      <p>Orient'Info est un site qui vous permet de trouver des informations sur votre orientation</p>
+
+      <p>retrouvez ici des témoignages, des dates de porte-ouvertes ou des adresses sur les filières et écoles qui vous intéressent</p>
+
+      </section>
+
+  </body>
+
+</html>
