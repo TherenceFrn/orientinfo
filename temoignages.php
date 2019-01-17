33ec9567392fba40dc8fcf46171b78f3749be8d5
FERRON Therence <therence.ferron@orange.fr>
2019-01-17 16:45:40 +0100

    fin de plein de trucs

---
+++ temoignages.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title>Orient'Info</title>
+
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+
+  </head>
+
+  <body>
+
+    <div id="temoignages">
+
+      <header class="header">
+
+      <nav>
+
+        <ul class="navigation">
+
+          <li class="navigation-bouton"><a href="index.php">Acceuil</a></li>
+
+          <li class="navigation-bouton"><a href="magazine.php">Magazine</a></li>
+
+          <li class="navigation-bouton"><a href="filiere.php">Les Filières</a></li>
+
+          <li class="navigation-bouton"><a href="ecole.php">Les Ecoles</a></li>
+
+          <li class="navigation-bouton"><a href="#temoignages.php">Temoignages</a></li>
+
+          <li class="navigation-bouton"><a href="contact.php">Contact</a></li>
+
+          <li class="navigation-bouton"><a href="etude.php">Etudes</a></li>
+
+          <li class="navigation-bouton"><a href="profil.php">Profil</a></li>
+
+        </ul>
+
+      </nav>
+
+      </header>
+
+      <main>
+
+      <section class="temoignage-1 magazine-prem">
+
+      <article>
+
+        <p><strong>Ecole :</strong> MyDigitalSchool </p>
+
+        <p><strong>Filière :</strong> Bachelor </p>
+
+        <p><strong>Description :</strong> blablablablablbalbalbalbaba<br>blablablablablablabla</p>
+
+        <p><strong>Date :</strong> 12/10/2019</p>
+
+        <p><strong>Lien :</strong> @ecole</p>
+
+      </article>
+
+      <aside>
+
+        <video width="600" height="337" controls="controls">
+
+          <source src="content/videos/video1.mp4" type="video/mp4">
+
+            <p>MyDigitalSchool</p>
+
+        </video>
+
+        <div>
+
+          <p>MyDigitalSchool</p>
+
+        </div>
+
+      </aside>
+
+      </section>
+
+      <section class="temoignage-2">
+
+      <aside>
+
+        <video width="600" height="337" controls="controls">
+
+          <source src="content/videos/video1.mp4" type="video/mp4">
+
+            <p>MyDigitalSchool</p>
+
+        </video>
+
+        <div>
+
+          <p>MyDigitalSchool</p>
+
+        </div>
+
+      </aside>
+
+      <article>
+
+        <p><strong>Ecole :</strong> MyDigitalSchool </p>
+
+        <p><strong>Filière :</strong> Bachelor </p>
+
+        <p><strong>Description :</strong> blablablablablbalbalbalbaba<br>blablablablablablabla</p>
+
+        <p><strong>Date :</strong> 12/10/2019</p>
+
+        <p><strong>Lien :</strong> @ecole</p>
+
+      </article>
+
+      </section>
+
+      <section class="temoignage-3">
+
+      <article>
+
+        <p><strong>Ecole :</strong> MyDigitalSchool </p>
+
+        <p><strong>Filière :</strong> Bachelor </p>
+
+        <p><strong>Description :</strong> blablablablablbalbalbalbaba<br>blablablablablablabla</p>
+
+        <p><strong>Date :</strong> 12/10/2019</p>
+
+        <p><strong>Lien :</strong> @ecole</p>
+
+      </article>
+
+      <aside>
+
+        <video width="600" height="337" controls="controls">
+
+          <source src="content/videos/video1.mp4" type="video/mp4">
+
+            <p>MyDigitalSchool</p>
+
+        </video>
+
+        <div>
+
+          <p>MyDigitalSchool</p>
+
+        </div>
+
+      </aside>
+
+      </section>
+
+      <section class="temoignage-4">
+
+      <aside>
+
+        <video width="600" height="337" controls="controls">
+
+          <source src="content/videos/video1.mp4" type="video/mp4">
+
+            <p>MyDigitalSchool</p>
+
+        </video>
+
+        <div>
+
+          <p>MyDigitalSchool</p>
+
+        </div>
+
+      </aside>
+
+      <article>
+
+        <p><strong>Ecole :</strong> MyDigitalSchool </p>
+
+        <p><strong>Filière :</strong> Bachelor </p>
+
+        <p><strong>Description :</strong> blablablablablbalbalbalbaba<br>blablablablablablabla</p>
+
+        <p><strong>Date :</strong> 12/10/2019</p>
+
+        <p><strong>Lien :</strong> @ecole</p>
+
+      </article>
+
+      </section>
+
+      </main>
+
+      <footer>
+
+        <ul class="footer">
+
+          <li class="footer-bouton">Orient'Info</li>
+
+          <li class="footer-bouton">Nous contacter</li>
+
+          <li class="footer-bouton">CGU</li>
+
+          <li class="footer-bouton">Réseaux Sociaux</li>
+
+          <li class="footer-bouto">
+
+          <ul class="reso-footer">
+
+          <li><a href="#" id="reso-1">Twitter</a></li>
+
+          <li><a href="#" id="reso-2">Facebook</a></li>
+
+          <li><a href="#" id="reso-3">Instagram</a></li>
+
+          <li><a href="#" id="reso-4">LinkedIn</a></li>
+
+          </ul>
+
+          </li>
+
+        </ul>
+
+      </footer>
+
+    </div>
+
+  </body>
+
+</html>
